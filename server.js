b3dc5f327e7d46338e748991915ffad2930fe0a0
Johnny Chen <Johnnychen384@gmail.com>
2023-03-06 13:37:04 -0500

    added route to look for restaurant by id

--- server.js
+++ server.js
@@ -6,10 +6,16 @@ const {sequelize} = require("./db");
 const port = 3000;
 
 //TODO: Create your GET Request Route Below: 
+app.get("/restaurants/:id", async (req, res) => {
+    const target = await Restaurant.findByPk(req.params.id)
+    res.json(target)
+})
+
 app.get("/restaurants", async (req, res) => {
     const restArr = await Restaurant.findAll()
     res.json(restArr)
 })
+
 
 
 app.listen(port, () => {
