463338ac06cd8542f6e67064f81a35661b65c54e
Johnny Chen <Johnnychen384@gmail.com>
2023-03-06 11:55:09 -0500

    finished get route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const {sequelize} = require("./db");
 const port = 3000;
 
 //TODO: Create your GET Request Route Below: 
+app.get("/restaurants", async (req, res) => {
+    const restArr = await Restaurant.findAll()
+    res.json(restArr)
+})
 
 
 app.listen(port, () => {
